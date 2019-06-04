aa7a545718aec06f8da3605bfdb6579ae06e5417
alessiosantangelo <alessio.santangelo@prima.it>
2019-06-04 14:36:32 +0200

    Loader Component (#7)

---
+++ src/Prima/Pyxis/Loader/Examples/Model.elm
@@ -0,0 +1,18 @@
+module Prima.Pyxis.Loader.Examples.Model exposing
+    ( Model
+    , Msg(..)
+    , initialModel
+    )
+
+
+type alias Model =
+    {}
+
+
+initialModel : Model
+initialModel =
+    Model
+
+
+type Msg
+    = NoOp
