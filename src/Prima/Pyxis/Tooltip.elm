75151d798b672639558eb1ee77b5c19f4382de83
Giuseppe Scuglia <peppescg@gmail.com>
2019-06-05 17:03:42 +0200

    Tooltips (#8)

---
+++ src/Prima/Pyxis/Tooltip.elm
@@ -0,0 +1,110 @@
+module Prima.Pyxis.Tooltip exposing
+    ( Config
+    , upConfig, downConfig, leftConfig, rightConfig
+    , render
+    )
+
+{-| Creates a Tooltip component by using predefined Html syntax.
+
+
+# Configuration
+
+@docs Config
+
+
+# Configuration Helpers
+
+@docs upConfig, downConfig, leftConfig, rightConfig
+
+
+# Render
+
+@docs render
+
+-}
+
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (classList)
+
+
+{-| Represents the config of a Tooltip
+-}
+type Config msg
+    = Config Configuration
+
+
+type alias Configuration =
+    { type_ : TooltipType
+    , content : String
+    }
+
+
+type TooltipType
+    = Up
+    | Down
+    | Left
+    | Right
+
+
+{-| Defines the configuration of an Up tooltip.
+-}
+upConfig : String -> Config msg
+upConfig content =
+    Config (Configuration Up content)
+
+
+{-| Defines the configuration of a Down tooltip.
+-}
+downConfig : String -> Config msg
+downConfig content =
+    Config (Configuration Down content)
+
+
+{-| Defines the configuration of an Left tooltip.
+-}
+leftConfig : String -> Config msg
+leftConfig content =
+    Config (Configuration Left content)
+
+
+{-| Defines the configuration of an Right tooltip.
+-}
+rightConfig : String -> Config msg
+rightConfig content =
+    Config (Configuration Right content)
+
+
+isTooltipUp : TooltipType -> Bool
+isTooltipUp =
+    (==) Up
+
+
+isTooltipDown : TooltipType -> Bool
+isTooltipDown =
+    (==) Down
+
+
+isTooltipLeft : TooltipType -> Bool
+isTooltipLeft =
+    (==) Left
+
+
+isTooltipRight : TooltipType -> Bool
+isTooltipRight =
+    (==) Right
+
+
+{-| Renders the Tooltip by receiving it's Config.
+-}
+render : Config msg -> Html msg
+render (Config config) =
+    div
+        [ classList
+            [ ( "tooltip", True )
+            , ( "tooltip--up", isTooltipUp config.type_ )
+            , ( "tooltip--down", isTooltipDown config.type_ )
+            , ( "tooltip--left", isTooltipLeft config.type_ )
+            , ( "tooltip--down", isTooltipRight config.type_ )
+            ]
+        ]
+        [ text config.content ]
---
+++ src/Prima/Pyxis/Tooltip/Example.elm
@@ -0,0 +1,77 @@
+module Prima.Pyxis.Tooltip.Example exposing
+    ( Model
+    , Msg(..)
+    , appBody
+    , init
+    , initialModel
+    , main
+    , update
+    , view
+    )
+
+import Browser
+import Html exposing (Html, div)
+import Html.Attributes exposing (class, style)
+import Prima.Pyxis.Helpers as Helpers
+import Prima.Pyxis.Tooltip as Tooltip
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel, Cmd.none )
+
+
+type alias Model =
+    {}
+
+
+initialModel : Model
+initialModel =
+    Model
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Browser.Document Msg
+view model =
+    Browser.Document "Tooltip component" (appBody model)
+
+
+appBody : Model -> List (Html Msg)
+appBody _ =
+    [ Helpers.pyxisStyle
+    , div
+        [ class "a-container a-container--medium directionColumn"
+        , style "position" "relative"
+        ]
+        ([ Tooltip.upConfig
+         , Tooltip.downConfig
+         , Tooltip.leftConfig
+         , Tooltip.rightConfig
+         ]
+            |> List.map tooltipBuilder
+            |> List.intersperse Helpers.spacer
+        )
+    ]
+
+
+tooltipBuilder : (String -> Tooltip.Config msg) -> Html msg
+tooltipBuilder mapper =
+    (Tooltip.render << mapper) "Lorem ipsum dolor sit amet."
