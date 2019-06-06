fff7e9ce46d919158fdf31f926ab0e55de9e40ba
Giuseppe Scuglia <peppescg@gmail.com>
2019-06-06 16:44:02 +0200

    Componente Separator (#10)

---
+++ src/Prima/Pyxis/Separator.elm
@@ -0,0 +1,11 @@
+module Prima.Pyxis.Separator exposing (render)
+
+import Html exposing (Html, hr)
+import Html.Attributes exposing (..)
+
+
+render : Html msg
+render =
+    hr
+        []
+        []
